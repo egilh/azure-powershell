96648824ebf9eb459be1660785de063822626560
Egil Haugli <egilrs@gmail.com>
2017-06-06 18:05:15 +0200

    fixed some typos made script more readable added more checks

--- AutomationAccounts/CreateSnapshotFromBlob.ps1
+++ AutomationAccounts/CreateSnapshotFromBlob.ps1
@@ -3,7 +3,7 @@
 Script made for use in Azure Automation Accounts.
 Creates snapshots of all VHD's in specified storage account 
 and copies snapshots to new storage container.
-Deletes snapshots on source-container if older then 1 day.
+Deletes snapshots on source container if older then 2 day.
 #>
 Import-Module -Name AzureRM.Compute
 Import-Module -Name AzureRM.Resources
@@ -21,65 +21,77 @@ $dstAccountName = Get-AutomationVariable -Name 'dstAccountName'
 $dstKey = Get-AutomationVariable -Name 'dstKey'
 $dstContainerName = 'vhd-snapshots'
 $dstContext = New-AzureStorageContext -StorageAccountName $dstAccountName -StorageAccountKey $dstKey
-  
-# Connect to ARM Resources
-$connectionName = "AzureRunAsConnection"
-  
-try {
-    # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
-
-    "Logging in to Azure..."
-    Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
-    Set-AzureRmContext -SubscriptionId $SubId
-}
-catch {
-    if (!$servicePrincipalConnection) {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-    }
-    else {
-        Write-Error -Message $_.Exception
-        throw $_.Exception
-    }
-}
 
 function CreateSnapshotFromBlob {
+    # Connect to ARM Resources
+    $connectionName = "AzureRunAsConnection"
+  
+    try {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+
+        "Logging in to Azure..."
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+        Set-AzureRmContext -SubscriptionId $SubId
+    }
+    catch {
+        if (!$servicePrincipalConnection) {
+            $ErrorMessage = "Connection $connectionName not found."
+            throw $ErrorMessage
+        }
+        else {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+
     # Get Blob reference and create snapshots of VHD's
     $blobs = Get-AzureStorageBlob -Container $srcContainerName -Context $srcContext | Where-Object {$_.Name -like '*.vhd'}
     foreach ($blob in $blobs) {
-        $blob.ICloudBlob.CreateSnapshot()
-        Write-Host "Creating snapshot of " + $blob.Name
-    }
-    # Copy snapshot to new container
-    $container = Get-AzureStorageContainer -Name $srcContainerName -Context $srcContext
-    $listOfBlobs = $container.CloudBlobContainer.ListBlobs($BlobName, $true, "Snapshots")
-
-    foreach ($CloudBlockBlob in $listOfBlobs) {
-        if ($CloudBlockBlob.IsSnapshot) {
-            $CloudBlockBlob.FetchAttributes()
-            $TimeDate = Get-Date -Format dd-MM-yyyy
-            $newBlobName = $CloudBlockBlob.Metadata["MicrosoftAzureCompute_VMName"] + "-$TimeDate.vhd"
-            $blobCopy = Start-AzureStorageBlobCopy -CloudBlob $CloudBlockBlob -DestContainer $dstContainerName -DestBlob $newBlobName -Context $dstContext -Force
-            $status = Get-AzureStorageBlobCopyState -CloudBlob $blobCopy.ICloudBlob -Context $dstContext
-            while ($status.Status -eq "Pending") {
-                Start-Sleep -Seconds 10
-                Write-Host "Status of $blobCopy.ICloudBlob  is " + $status.Status
-            }
+        if (!$blob.CloudBlob.IsSnapshot) {
+            $blob.CloudBlob.CreateSnapshot()
+            Write-Host "Creating snapshot of " $blob.Name
         }
     }
-    # Delete old snapshots if older then 1 day
+
+    # Copy snapshot to new container
+    $dstContainer = Get-AzureStorageContainer -Name $dstContainerName -Context $dstContext
+    if (!$dstContainer){
+        New-AzureStorageContainer -Name $dstContainerName -Context $dstContext
+    }
+    $container = Get-AzureStorageContainer -Name $srcContainerName -Context $srcContext
+    $listOfBlobs = $container.CloudBlobContainer.ListBlobs($BlobName, $true, "Snapshots")
+    foreach ($CloudBlockBlob in $listOfBlobs) {
+        $CloudBlockBlob.FetchAttributes()
+        if ($CloudBlockBlob.IsSnapshot) {
+            $TimeDate = Get-Date -Format dd-MM-yyyy
+            $newBlobName = $($CloudBlockBlob.Metadata["MicrosoftAzureCompute_DiskName"] + "-$TimeDate.vhd")
+            $copyBlob = Start-AzureStorageBlobCopy `
+            -CloudBlob $CloudBlockBlob `
+            -DestContainer $dstContainerName `
+            -DestBlob $newBlobName `
+            -Context $dstContext `
+            -Force
+            $status = $copyBlob | Get-AzureStorageBlobCopyState
+            if ($status.Status -eq "Pending") {
+                Start-Sleep -Seconds 10
+                Write-Host $($CloudBlockBlob.Metadata["MicrosoftAzureCompute_DiskName"] + " is " + $status.Status)
+            } 
+        }
+    }
+
+    # Delete old snapshots if older then 2 days
+    # Should involve more advanced checking
     Write-Host "Checking for old snapshots .."
     foreach ($CloudBlockBlob in $ListOfBLobs) {
-        if ($CloudBlockBlob.IsSnapshot -and $SnapShotTime -le (Get-Date).AddDays(-1)) {
+        $CloudBlockBlob.FetchAttributes()
+        if ($CloudBlockBlob.IsSnapshot -and $CloudBlockBlobSnapshot.SnapshotTime.Date -le (Get-Date).AddDays(-2)) {
             $CloudBlockBlobSnapshot = $CloudBlockBlob
-            $SnapShotTime = $CloudBlockBlobSnapshot.SnapshotTime.Date
-            $CloudBlockBlob.FetchAttributes()
-            Write-Host "Deleting old snapshot of  " $CloudBlockBlobSnapshot.Metadata["MicrosoftAzureCompute_VMName"]
+            Write-Host "Deleting old snapshot of  " $CloudBlockBlob.Metadata["MicrosoftAzureCompute_DiskName"]
             $CloudBlockBlobSnapshot.SnapshotTime
             $CloudBlockBlobSnapshot.Delete() 
         }
